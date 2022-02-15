b0782ff00179b7422ee8ce420d046af7719a7a82
BenNorsk <benjamin.brueckner@bluewin.ch>
2022-02-15 17:03:41 +0100

    added documents

--- academics_data.py
+++
@@ -1,4 +0,0 @@
-# Here comes the academics data
-
-def get_academics_data():
-    data = {"content": "here comes the academics data."}
---
+++ document_data.py
@@ -0,0 +1,21 @@
+# Here we get the data about the documents
+
+def get_documents():
+    documents =  {
+        "constitution": {
+            "title": "Constitution of the Crypto Society St. Gallen",
+            "publication_date": "12.12.2021",
+            "link": "tbd"
+        },
+        "general_assembly_2022": {
+            "title": "General Assembly 2022",
+            "publication_date": "08.02.2022",
+            "link": "tbd"
+        },
+        "bitcoin_whitepaper": {
+            "title": "Bitcoin Whitepaper",
+            "publication_date": "31.10.2008",
+            "link": "tbd"
+        }
+    }
+    return documents
