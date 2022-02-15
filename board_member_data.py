c1a40fe4499f7ea57179fc6e9728666feb86ed17
BenNorsk <benjamin.brueckner@bluewin.ch>
2022-02-15 18:03:40 +0100

    resolved the links

--- board_member_data.py
+++ board_member_data.py
@@ -1,93 +1,95 @@
-def get_board_members():
+# This function returns all board members
+
+def get_board_members(base_url):
     board_members = {
     "president": {
         "title": "President",
         "name": "Benjamin L. Brückner",
-        "image": "president.jpeg",
-        "board_member_image": "images/board_member_images/president.jpeg",
+        "image": f'{base_url}images/position_images/president.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/president.jpeg',
         "short_description": "As President, I lead and assist the Board in growing and designing the society each and every day.",
         "email": "president@cryptosocietystgallen.club"
         },
     "vice_president": {
         "title": "Vice-President",
         "name": "Gian O. Mühlemann",
-        "image": "vice-president.jpeg",
-        "board_member_image": "images/board_member_images/vice-president.jpg",
+        "image": f'{base_url}images/position_images/vice-president.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/vice-president.jpg',
         "short_description": "As Vice-President, I support and advise the president in his operative and strategic tasks. Together we lead and assist the Board in creating a growing cryptosociety.",
         "email": "vice-president@cryptosocietystgallen.club"
     },
     "head_of_legal_affairs": {
         "title": "Head of Legal Affairs",
         "name": "Jan Scherrer",
-        "image": "legal-affairs.jpeg",
-        "board_member_image": "images/board_member_images/legal-affairs.jpg",
+        "image": f'{base_url}images/position_images/legal-affairs.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/legal-affairs.jpg',
         "short_description": "As Head of Legal Affairs, my responsibilities include inter alia the creation and management of all legal documents, the society’s official incorporation, and the appointment of the Judicial Committee.",
         "email": "legal-affairs@cryptosocietystgallen.club"},
     "head_of_crypto_projects": {
         "title": "Head of Crypto Projects",
         "name": "Robert Jacobs",
-        "image": "legal-affairs.jpeg",
-        "board_member_image": "images/board_member_images/legal-affairs.jpg",
+        "image": f'{base_url}images/position_images/legal-affairs.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/legal-affairs.jpg',
         "short_description": "Coming soon.",
         "email": "crypto-projects@cryptosocietystgallen.club"},
     "chief_of_staff": {
         "title": "Chief of Staff",
         "name": "Sanna Mühlemann",
-        "image": "staff.jpeg",
-        "board_member_image": "images/board_member_images/staff.png",
+        "image": f'{base_url}images/position_images/staff.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/staff.png',
         "short_description": "As Chief of Staff, I am your first point of contact. I will make sure to help you if there are any issues or questions regarding to our society.",
         "email": "chief-of-staff@cryptosocietystgallen.club"},
     "vice_chief_of_staff": {
         "title": "Vice Chief of Staff",
         "name": "Mariska–Morgaine Rüger",
-        "image": "staff.jpeg",
-        "board_member_image": "images/board_member_images/staff.png",
+        "image": f'{base_url}images/position_images/staff.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/staff.png',
         "short_description": "As Vice Chief of Staff, I am your first point of contact. I will make sure to help you if there are any issues or questions regarding to our society.",
         "email": "chief-of-staff@cryptosocietystgallen.club"},
     "head_of_finance": {
         "title": "Head of Finance",
         "name": "Elias Henke",
-        "image": "finance.jpg",
-        "board_member_image": "images/board_member_images/finance.jpg",
+        "image": f'{base_url}images/position_images/finance.jpg',
+        "board_member_image": f'{base_url}images/board_member_images/finance.jpg',
         "short_description": "As Head of Finance, I am responsible for managing all financial affairs of the society as well as for the development of the finance department.",
         "email": "finance@cryptosocietystgallen.club"
     },
     "head_of_academic_affairs1": {
         "title": "Co–Head of Academic Affairs",
         "name": "Malte Knauer",
-        "image": "academic-affairs.jpeg",
-        "board_member_image": "images/board_member_images/academic-affairs.jpg",
+        "image": f'{base_url}images/position_images/academic-affairs.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/academic-affairs.jpg',
         "short_description": "As Co–Head of Academic Affairs, I develop and plan how knowledge about crypto is shared throughout the society. My goal is to both let beginners and advanced students emerse into the realm of Crypto & Blockchain.",
         "email": "academic-affairs@cryptosocietystgallen.club"
     },
     "head_of_academic_affairs2": {
         "title": "Co–Head of Academic Affairs",
         "name": "Victor Hoppe",
-        "image": "academic-affairs.jpeg",
-        "board_member_image": "images/board_member_images/academic-affairs.jpg",
+        "image": f'{base_url}images/position_images/academic-affairs.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/academic-affairs.jpg',
         "short_description": "Coming soon.",
         "email": "academic-affairs@cryptosocietystgallen.club"
     },
     "head_of_marketing": {
         "title": "Head of Marketing",
         "name": "Sandro Gössi",
-        "image": "marketing.jpeg",
-        "board_member_image": "images/board_member_images/marketing.jpg",
+        "image": f'{base_url}images/position_images/marketing.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/marketing.jpg',
         "short_description": "As Head of Marketing, I lead all marketing activities from social media, branding and digital campaigns to advertising and creative projects. Your vibe attracts your tribe!",
         "email": "marketing@cryptosocietystgallen.club"
     },
     "head_of_events1": {
         "title": "Co–Head of Events",
         "name": "Davide Brunetti",
-        "image": "socials.jpeg",
-        "board_member_image": "images/board_member_images/socials.jpg",
+        "image": f'{base_url}images/position_images/socials.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/socials.jpg',
         "short_description": "As Co–Head of Events, I contribute to the social platform of the society and I am determined to establish a pleasant environment of social exchange.",
         "email": "social-events@cryptosocietystgallen.club"},
     "head_of_events2": {
         "title": "Co–Head of Events",
         "name": "Andre Lubawin",
-        "image": "socials.jpeg",
-        "board_member_image": "images/board_member_images/socials.jpg",
+        "image": f'{base_url}images/position_images/socials.jpeg',
+        "board_member_image": f'{base_url}images/board_member_images/socials.jpg',
         "short_description": "As Co–Head of Events, I contribute to the social platform of the society and I am determined to establish a pleasant environment of social exchange.",
         "email": "events@cryptosocietystgallen.club"}
     }
--- document_data.py
+++ document_data.py
@@ -1,21 +1,21 @@
 # Here we get the data about the documents
 
-def get_documents():
+def get_documents(base_link):
     documents =  {
         "constitution": {
             "title": "Constitution of the Crypto Society St. Gallen",
             "publication_date": "12.12.2021",
-            "link": "tbd"
+            "link": f'{base_link}documents/constitution.txt'
         },
         "general_assembly_2022": {
             "title": "General Assembly 2022",
             "publication_date": "08.02.2022",
-            "link": "tbd"
+            "link": f'{base_link}documents/ga.txt'
         },
         "bitcoin_whitepaper": {
             "title": "Bitcoin Whitepaper",
             "publication_date": "31.10.2008",
-            "link": "tbd"
+            "link": f'{base_link}documents/bitcoin_whitepaper.pdf'
         }
     }
     return documents
