5923213fa3e361307422a4950cd533cb57fa5a2f
BenNorsk <benjamin.brueckner@bluewin.ch>
2022-02-18 17:21:56 +0100

    hopefully no changes

--- posts_data.py
+++ posts_data.py
@@ -12,6 +12,33 @@ def get_posts_data(base_url):
                 "author": "Benjamin L. Brückner",
                 "content": "On the 22.12.2021, our first official event took place. We gathered with nearly 30 people in the Einstein Hotel, and held an introduction into the topic of Crypto, and into the Crypto Society of St. Gallen. We discussed, debated, lectured, socialised and laughed together at our very first X-Mas event! Feel free to check out some more pictures to it on our Instagram post.",
                 "link": "https://www.instagram.com/p/CX68HwTNLfj/"
+            },
+        "x-mas2":
+            {
+                "title": "Review our X-Mas Event!",
+                "icon": f'{base_url}images/post_images/x-mas-post/christmas.jpg',
+                "image": f'{base_url}images/post_images/x-mas-post/xmas2.jpg',
+                "author": "Benjamin L. Brückner",
+                "content": "On the 22.12.2021, our first official event took place. We gathered with nearly 30 people in the Einstein Hotel, and held an introduction into the topic of Crypto, and into the Crypto Society of St. Gallen. We discussed, debated, lectured, socialised and laughed together at our very first X-Mas event! Feel free to check out some more pictures to it on our Instagram post.",
+                "link": "https://www.instagram.com/p/CX68HwTNLfj/"
+            },
+            "x-mas":
+            {
+                "title": "Review our X-Mas Event!",
+                "icon": f'{base_url}images/post_images/x-mas-post/christmas.jpg',
+                "image": f'{base_url}images/post_images/x-mas-post/xmas1.jpg',
+                "author": "Benjamin L. Brückner",
+                "content": "On the 22.12.2021, our first official event took place. We gathered with nearly 30 people in the Einstein Hotel, and held an introduction into the topic of Crypto, and into the Crypto Society of St. Gallen. We discussed, debated, lectured, socialised and laughed together at our very first X-Mas event! Feel free to check out some more pictures to it on our Instagram post.",
+                "link": "https://www.instagram.com/p/CX68HwTNLfj/"
+            },
+            "x-mas":
+            {
+                "title": "Review our X-Mas Event!",
+                "icon": f'{base_url}images/post_images/x-mas-post/christmas.jpg',
+                "image": f'{base_url}images/post_images/x-mas-post/xmas1.jpg',
+                "author": "Benjamin L. Brückner",
+                "content": "On the 22.12.2021, our first official event took place. We gathered with nearly 30 people in the Einstein Hotel, and held an introduction into the topic of Crypto, and into the Crypto Society of St. Gallen. We discussed, debated, lectured, socialised and laughed together at our very first X-Mas event! Feel free to check out some more pictures to it on our Instagram post.",
+                "link": "https://www.instagram.com/p/CX68HwTNLfj/"
             }
     }
     return posts
