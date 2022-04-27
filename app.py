286a862a2771726cd109057512d183eb9e7c41e0
BenNorsk <benjamin.brueckner@bluewin.ch>
2022-04-27 14:00:41 +0200

    added partners to api

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from module import clean_data, get_brand_data
 from board_member_data import get_board_members
 from posts_data import get_posts_data
 from document_data import get_documents
+from partners_data import get_partners_data
 from werkzeug.utils import secure_filename
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -13,24 +14,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 prices = {"ADA":{"change":"down","daily_price_change":"-0.0025","id":"ADA","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/ada.svg","market_cap_dominance":"0.0169","name":"Cardano","price":"1.01"},"AVAX":{"change":"up","daily_price_change":"0.0565","id":"AVAX","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/AVAX.svg","market_cap_dominance":"0.0113","name":"Avalanche","price":"88.11"},"BNB":{"change":"up","daily_price_change":"0.0031","id":"BNB","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/bnb.svg","market_cap_dominance":"0.0350","name":"Binance Coin","price":"397.71"},"BTC":{"change":"up","daily_price_change":"0.0004","id":"BTC","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg","market_cap_dominance":"0.4058","name":"Bitcoin","price":"40838.48"},"ETH":{"change":"up","daily_price_change":"0.0131","id":"ETH","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/eth.svg","market_cap_dominance":"0.1828","name":"Ethereum","price":"2916.46"},"SOL":{"change":"up","daily_price_change":"0.0099","id":"SOL","logo_url":"https://nomics-api.s3.us-east-2.amazonaws.com/static/images/currencies/SOL2.jpg","market_cap_dominance":"0.0159","name":"Solana","price":"94.95"},"XRP":{"change":"up","daily_price_change":"0.0129","id":"XRP","logo_url":"https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/XRP.svg","market_cap_dominance":"0.0196","name":"XRP","price":"0.78"}}
 
-# def update_prices():
-#     api_key = 'ab4d9d5a55d9600d15ca26450ba9e6533dc496a5'
-#     url = f'https://api.nomics.com/v1/currencies/ticker?key={api_key}&ids=BTC,ETH,ADA,SOL,XRP,BNB,AVAX&interval=1d,30d&convert=USD'
-#     try:
-#         response = requests.get(url)
-#         data = response.json()
-#         data = clean_data(data)
-#         prices = data
-#         print("updated prices!")
-#         return "success"
-#     except:
-#         print("error")
-#         return "error"
-
-# update_prices()
-# sched = BackgroundScheduler(daemon=True)
-# sched.add_job(update_prices,'interval',minutes=120)
-# sched.start()
 
 # ––––––––––––– Get prices –––––––––––––
 
@@ -71,6 +54,7 @@ def get_prices():
     # git add .
     # git commit -m "finished api key tasks"
     # git push origin api-keys
+
     api_key = 'ab4d9d5a55d9600d15ca26450ba9e6533dc496a5'
     url = f'https://api.nomics.com/v1/currencies/ticker?key={api_key}&ids=BTC,ETH,ADA,SOL,XRP,BNB,AVAX&interval=1d,30d&convert=USD'
     try:
@@ -100,8 +84,9 @@ def welcome():
     board_data = get_board_members(base_url)
     posts_data = get_posts_data(base_url)
     documents_data = get_documents(base_url)
+    partners_data = get_partners_data(base_url)
     standard_data = {"title": "Crypto Society St. Gallen", 
-            "welcome_text": "The Crypto Society is the number one address for crypto enthusiastic students in St. Gallen! Join up to meet like–minded people, deepen your knowledge about crypto and emerge in this world-changing industry!",
+            "welcome_text": "The Crypto Society is the number one address for crypto enthusiastic students in St. Gallen! Join up to meet like–minded people, deepen your knowledge about crypto and emerge in this world-changing industry! In weekly meetings, and regular events with bigger companies and startups, you may learn plenty about the future of the blockchain and crypto technology. The Crypto Society will connect you perfectly with the industry, and serve as your starting point for a future in the future of finance! Also of course, never forget to miss out on our social events of the great crypto community!",
             "member_link": "https://docs.google.com/forms/d/e/1FAIpQLSeP7INDPgUYb8nCraOel61WOgFn48dnii5fDMLcyTTBI8XIeg/viewform",
             "linked_in": "https://www.linkedin.com/company/cryptosocietystgallen/mycompany/",
             "instagram": "https://www.instagram.com/cryptosocietystgallen/",
@@ -116,7 +101,8 @@ def welcome():
             "prices": {**get_prices()},
             "posts": posts_data,
             "documents": {**documents_data},
-            "board": board_data   
+            "board": board_data  ,
+            "partners": partners_data
     }
     data = {**brand_data, **standard_data}
     return data
---
+++ partners_data.py
@@ -0,0 +1,14 @@
+def get_partners_data(base_url):
+    partners = [
+        {
+                "name": "4bridges",
+                "icon": f'{base_url}images/partners_images/4bridges.png',
+                "url": "https://4bridges.ch/"
+        },
+        {
+                "name": "Die Ostschweiz",
+                "icon": f'{base_url}images/partners_images/die_ostschweiz.png',
+                "url": "https://www.dieostschweiz.ch/"
+        }
+    ]
+    return partners
